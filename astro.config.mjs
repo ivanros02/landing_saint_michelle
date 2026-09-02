a40c74794aa252083d68099a656768de742cf426
ivanros02 <ivanrosendo1102@gmail.com>
2026-09-02 11:10:23 -0300

    Agrega config de site y base para rutas de assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ivanros02.github.io',
+  base: '/landing_saint_michelle',
   vite: {
     plugins: [tailwindcss()]
   }
